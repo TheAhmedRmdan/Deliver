4438801f24cf9f7951c87a8c938362e221b8199f
Ahmed <theahmedrmdan@gmail.com>
2024-06-26 20:26:32 +0300

    Fixed data_editor bug

--- pages/Entry.py
+++ pages/Entry.py
@@ -14,12 +14,20 @@ def show_logout():
             st.switch_page("Login.py")
 
 
-show_logout()
-DSN = st.secrets.get("connections")["psycopg"]["DSN"]
-DB_URL = st.secrets.get("connections")["sqlalchemy"]["URL"]
+def get_data(table_name):
+    query = f"SELECT * FROM {table_name}"
+    postgres_df = pd.read_sql(query, DB_URL)
+    return postgres_df
 
+
+def commit_to_db(df: pd.DataFrame, tab_name):
+    df.to_sql(tab_name, DB_URL, if_exists="replace", index=False, dtype={"time": TIME})
+    st.success("Pushed to database sucessfully", icon="✅")
+
+
+DB_URL = st.secrets.get("connections")["sqlalchemy"]["URL"]
 NASR_AREAS = ["مكرم عبيد", "الزهراء", "العاشر", "السادس", "السابع"]
-col_config = {
+COL_CONFIG = {
     "area": st.column_config.SelectboxColumn("Area", options=NASR_AREAS),
     "gmap": st.column_config.LinkColumn("Maps URL"),
     "street": st.column_config.TextColumn("Street"),
@@ -27,14 +35,24 @@ col_config = {
     "whatsapp": st.column_config.LinkColumn("Whatsapp URL"),
 }
 
+show_logout()
 table_name = st.text_input("Database table name: ")
-if st.button("Connect to database"):
-    query = f"SELECT * FROM {table_name}"
-    postgres_df = pd.read_sql(query, DB_URL)
-    data = st.data_editor(postgres_df, hide_index=True, column_config=col_config)
+if "df" not in st.session_state:
+    st.session_state.df = None
 
-    if st.button("Submit to database"):
-        data.to_sql(
-            table_name, DB_URL, if_exists="replace", index=False, dtype={"time": TIME}
+if st.button("Fetch Data"):
+    st.write("Fetching data...")
+    st.session_state.df = get_data(table_name)
+
+if st.session_state.df is not None:
+    with st.form("Edit_form"):
+        edited_df = st.data_editor(
+            st.session_state.df, hide_index=True, column_config=COL_CONFIG
         )
-        st.success("Pushed to database sucessfully", icon="✅")
+        submitted = st.form_submit_button("Submit modified data")
+        if submitted:
+            commit_to_db(edited_df, table_name)
+            # Clear the session state after sending
+            st.session_state.df = None
+else:
+    st.write("No data fetched yet.")
